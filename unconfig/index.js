68817dea08dc614b62804a1e04fac5aebc53b6a2
bluwy <bjornlu.dev@gmail.com>
2024-11-07 17:11:25 +0800

    Use @bluwy/unconfig

--- unconfig/index.js
+++
@@ -1,6 +0,0 @@
-// Unbloated version of unconfig that simply imports the config with `import()`
-
-export async function loadConfig() {
-  const config = (await import('../sponsor.config.js')).default
-  return { config }
-}
